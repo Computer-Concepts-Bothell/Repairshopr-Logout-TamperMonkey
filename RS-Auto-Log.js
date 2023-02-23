eed9977986be56d4fc194a85a0951cc941e2c8c3
Pixelbays <115045731+Pixelbays@users.noreply.github.com>
2023-02-23 12:45:11 -0800

    Update RS-Auto-Log.js
    add multitab support

--- RS-Auto-Log.js
+++ RS-Auto-Log.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         RS-Auto-Log
 // @namespace    https://github.com/Pixelbays/Repairshopr-Logout-TamperMonkey
-// @version      0.2.1
+// @version      0.3.0
 // @description  Auto Logs out of RepairShopr After 10 minutes
 // @author       HeruEdhel@Pixelbays
 // @include      https://*.repairshopr.com/*
@@ -10,9 +10,9 @@
 // @downloadURL  https://raw.githubusercontent.com/Pixelbays/Repairshopr-Logout-TamperMonkey/main/RS-Auto-Log
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=repairshopr.com
 // @supportURL   https://github.com/Pixelbays/Repairshopr-Logout-TamperMonkey/issues
-// @grant        none
+// @grant        GM_setValue
+// @grant        GM_getValue
 // ==/UserScript==
-
 (function() {
     // Set the time (in milliseconds) after which inactivity will be detected
     var inactivityTime = 600000; // 10 minutes
@@ -20,19 +20,36 @@
     // 600000; // 10 minutes
     // 1000; // 1 sec
     // 10000; // 10 secs
+    var executed = false
     // Set the function to be executed when inactivity is detected
     function inactivityDetected() {
-      lockScreenAndPinSwitch()
-      alert("Inactivity detected!");
+        if (!executed){
+            lockScreenAndPinSwitch();
+            alert("Inactivity detected!");
+            executed = true
+        }
     }
-
-    // Set a timer to detect inactivity
-    var inactivityTimer = setTimeout(inactivityDetected, inactivityTime);
 
     // Reset the timer whenever the user interacts with the page
     document.addEventListener("click", function() {
-      clearTimeout(inactivityTimer);
-      inactivityTimer = setTimeout(inactivityDetected, inactivityTime);
+        // Store the current time in a variable
+        var currentTime = new Date().getTime();
+        // Set the current time as the last active time for this tab
+        GM_setValue("lastActiveTime", currentTime);
+        executed = false
     });
 
+    // Set a timer to detect inactivity for each tab
+    setInterval(function() {
+        // Get the last active time for this tab
+        var lastActiveTime = GM_getValue("lastActiveTime", 0);
+        // Get the current time
+        var currentTime = new Date().getTime();
+        // Calculate the time difference between the last active time and the current time
+        var timeDiff = currentTime - lastActiveTime;
+        // If the time difference is greater than the inactivity time, call the inactivityDetected function
+        if (timeDiff > inactivityTime) {
+            inactivityDetected();
+        }
+    }, 1000);
 })();
